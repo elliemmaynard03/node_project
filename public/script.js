dfd568f966563b4e3942e88e609749930d0e7e62
elliemaynard03 <EMM38@email.sc.edu>
2023-11-05 17:06:58 -0500

    adding json

---
+++ public/script.js
@@ -0,0 +1,46 @@
+const showBrands = async() => {
+    const brandsJSON = await getBrands();
+    const brandsDiv = document.getElementById("brands-div");
+
+    if (brandsJSON == "") {
+        console.log("Invalid load of json");
+        return;
+    }
+
+    brandsJSON.forEach((brand) => {
+        const section = document.createElement("section");
+        brandsDiv.append(section);
+
+        const h3 = document.createElement("h3");
+        h3.innerHTML = brand.name;
+        h3.classList.add("heading");
+        section.append(h3);
+
+        const genres = document.createElement("p");
+        genres.innerHTML = brand.genres;
+        genres.classList.add("heading");
+        section.append(genres);
+
+        const owner = document.createElement("p");
+        owner.innerHTML = brand.owner;
+        owner.classList.add("heading");
+        section.append(owner);
+
+        const img = document.createElement("img");
+        img.src = "http://localhost:3000/" + brand.img;
+        img.classList.add("image");
+        section.append(img);
+    });
+
+}
+
+const getBrands = async() => {
+    try {
+        return (await fetch("http://localhost:3000/api/brands")).json();
+    } catch (error) {
+        console.log("error retrieving json");
+        return "";
+    }
+}
+
+window.onload = () => showBrands();
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+app.get("/api/brands", (req, res) => {
+    const brands = [];
+    brands[0] = {
+        name: "Rhode Skin",
+        genres: ["Skin Care", " Makeup"],
+        owner: "Hailey Bieber",
+        img: "images/rhodeskin.jpg"
+    };
+    brands[1] = {
+        name: "Skims",
+        genres: ["Lounge Clothing", " Going Out Clothing", " Everyday Clothing"],
+        owner: "Kim Kardashian",
+        img: "images/skims.jpg"
+    };
+    brands[2] = {
+        name: "One Size",
+        genres: ["Makeup", " Skin Care"],
+        owner: "Patrick Starr",
+        img: "images/onesize.jpg"
+    };
+    brands[3] = {
+        name: "Kylie Cosmetics",
+        genres: ["Makeup", " Skin Care"],
+        owner: "Kylie Jenner",
+        img: "images/kyliecosmetics.jpg"
+    };
+    brands[4] = {
+        name: "Fenty Beauty",
+        genres: ["Makeup", " Skin Care"],
+        owner: "Rihanna",
+        img: "images/fentybeauty.jpg"
+    };
+    brands[5] = {
+        name: "Drew House",
+        genres: ["Lounge Clothing", " Everyday Clothing"],
+        owner: "Justin Beiber",
+        img: "images/drewhouse.jpg"
+    };
+
+    res.json(brands);
+});
+
+app.listen(3000, () => {
+    console.log("listening");
+});
